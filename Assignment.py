2053a13963a7172d65882cfb1c9d21618d62ec4e
Sumit Soni <32272052+ssoni18@users.noreply.github.com>
2021-08-15 05:00:12 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,3 +1,4 @@
+from sys import stdin
 opcodes = {
     'add': '00000',
     'sub': '00001',
@@ -58,31 +59,148 @@ registers = {
 
 # Initial register values
 registers_values = {
-    'R0': 0000000000000000,
-    'R1': 0000000000000000,
-    'R2': 0000000000000000,
-    'R3': 0000000000000000,
-    'R4': 0000000000000000,
-    'R5': 0000000000000000,
-    'R6': 0000000000000000,
-    'FLAGS': 0000000000000000,
+    'R0': '0000000000000000',
+    'R1': '0000000000000000',
+    'R2': '0000000000000000',
+    'R3': '0000000000000000',
+    'R4': '0000000000000000',
+    'R5': '0000000000000000',
+    'R6': '0000000000000000',
+    'FLAGS': '0000000000000000',
 }
-
+line_number=1
+mem_address=0
 # Dictionary to store the values of the memory addresses
 variables = {}
+mem_address={}
+variables_and_labels={}  #its key will be the variable name and value will be the memoery address
+errors={}
+def check_errors(line):
+    global line_number
+    line_number+=1
+    global mem_address
+    lst=list(line.split(" "))
+    if(line=='\n'):
+        pass
+    else:
+        flag=0
+        if(lst[0]=='var'):
+            if(len(lst)!=2):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instruction(wrong number of arguments'
+            elif(lst[1][0]=='$'):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instructions(wrong type of argument is used'
+            elif(lst[1] in variables_and_labels):
+                flag+=1
+                errors[line_number]='Value has already been initialised earlier(both label and variable have same name)'
+            elif(lst[1] in registers):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instruction(wrong type of argument is used'
+            elif(lst[1] in opcodes):
+                flag+1
+                errors[line_number]='Wrong syntax used for instruction(wrong type of argument is used'                    
+        
+        elif(lst[0]=='add' or lst[0]=='sub' or lst[0]=='mov' or lst[0]=='ld' or lst[0]=='st' or lst[0]=='mul' or lst[0]=='div' or lst[0]=='rs' or lst[0]=='ls' or lst[0]=='xor' or lst[0]=='or' or lst[0]=='and' or lst[0]=='not' or lst[0]=='cmp' or lst[0]=='jmp' or lst[0]=='jlt' or lst[0]=='jgt' or lst[0]=='je' or lst[0]=='hlt'):
+            if(len(lst)!=4):   #to check the number of argumenst are correct or not
+                flag+=1
+                errors[line_number]='Wrong syntax used for instructions(wrong number of arguments)'
+            elif(lst[0]=='add' or lst[0]=='sub' or lst[0]=='mul' or lst[0]=='xor' or lst[0]=='or' or lst[0]=='and'):
+                if(lst[1][0]=='$' or lst[2][0]=='$' or lst[3][0]=='$' or lst[1] in opcodes or lst[2] in opcodes or lst[3] in opcodes):
+                    flag+=1
+                    errors[line_number]='Wrong syntax used for instructions'
+            else:
+                if(flag==0):
+                    mem_address+=1           
+        
+        elif(lst[0]=='rs' or lst[0]=='ls'):
+            if(len(lst)!=3):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instructions(wrong number of arguments)'
+            elif(lst[2][0]=='$'):
+                if(int(lst[2][1:])<=225 and int(lst[2][1:])>0):
+                    pass
+                else:
+                    flag+=1
+                    errors[line_number]='Illegal immidiate values'
+            elif(lst[2] in opcodes or lst[2] in registers or lst[1][0]=='$' or lst[1] in opcodes):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instructions'
+            else:
+                if(flag==0):
+                    mem_address+=1
+            
+        elif(lst[0]=='mov'):
+            if(len(lst)!=3):
+                flag+=1
+                errors[line_number]='Wrong syntax used for instructions(wrong number of arguments)'
+            elif(lst[2][0]=='$' and lst[1][0]=='$'):
+                flag+=1
+                errors[line_number]="wrong syntax used for instructions"
+            elif(lst[2][0]!='$' and lst[1][0]=='$'):
+                flag+=1
+                errors[line_number]="wrong syntax used for instructions"
+            elif(lst[2] not in opcodes and lst[1] not in opcodes):
+                if(flag==0):
+                    mem_address+=1
+                else:
+                    errors[line_number]="wrong syntax used for instructions"
+        
+        elif(lst[0]=='ld' or lst[0]=='st'):
+            if(len(lst)!=3):
+                flag+=1
+                errors[line_number]="wrong sysntax used for isntructions(wrong number of arguments are used"
+            if(lst[1] not in registers):
+                flag+=1
+                errors[line_number]="wrong syntax used for instructions"
+            if(lst[2] in variables_and_labels):
+                flag+=1
+                errors[line_number]="Value has already been initialised earlier"
+            if(lst[1][0]=='$' or lst[2][0]=='$' or lst[1] in opcodes or lst[2] in opcodes or lst[2] in registers):
+                flag+=1
+                errors[line_number]="Wrong syntax used for instructions"
+            else:
+                if(flag==0):
+                    mem_address+=1
+        
+        elif(lst[0]=='jmp' or lst[0]=='jlt' or lst[0]=='jgt' or lst[0]=='je'):
+            if(len(lst)!=2):
+                flag+=1
+                errors[line_number]="wrong sysntax used for isntructions(wrong number of arguments are used"
+            if(lst[1] in registers or lst[1][0]=='$' or lst[1] in opcodes):
+                flag+=1
+                errors[line_number]="wrong syntax used for instructions"
+            else:
+                if(flag==0):
+                    mem_address+=1
+        
+        elif(lst[0]=='hlt'):
+            if(len(lst)==1):
+                mem_address+=1
+            else:
+                errors[line_number]="Wrong syntax used(incorrect number of arguments)"
+        
+        else:
+            errors[line_number]='typos in instruction name'
+        
 
 
 # Convert a string decimal into the equivalent binary of custom bits
 def binary(number, bit):
     binary = bin(int(number)).replace('0b', '')[::-1]
+    if len(binary) > bit:
+        binary = binary[0:bit]
+        return binary[::-1]
+
     while len(binary) < bit:
         binary += '0'
     return binary[::-1]
 
-
 def process(command):
     s = ""
     words = list(command.split())
+    registers_values['FLAGS']='0000000000000000'
+    
     if words[0] != 'mov':
         if words[0] == 'var':  # Create a new variable and store a default value as 0.
             variables[words[1]] = 0000000000000000
@@ -95,16 +213,66 @@ def process(command):
                 s += '0' * (16 - 5 - 3 * types[opcodes[words[0]]])
                 for i in range(1, types[opcodes[words[0]]] + 1):
                     s += registers[words[i]]
-            elif words[0] == 'add':  # addition
-                registers_values[words[1]] = registers_values[words[2]] + registers_values[words[3]]
+            
+            if words[0] == 'add':  # addition
+                if int(registers_values[words[2]], 2) + int(registers_values[words[3]], 2) > 65535:
+                    flags = list(registers_values['FLAGS'])
+                    flags[12] = '1'
+                    registers_values['FLAGS'] = "".join(flags)
+                registers_values[words[1]] = binary(int(registers_values[words[2]], 2) + int(registers_values[words[3]], 2), 16)  # Store only 16 bits even for overflow
+
             elif words[0] == 'sub':  # subtraction
-                registers_values[words[1]] = registers_values[words[2]] - registers_values[words[3]]
+                if int(registers_values[words[2]], 2) - int(registers_values[words[3]], 2) < 0:
+                    flags = list(registers_values['FLAGS'])
+                    flags[12] = '1'
+                    registers_values['FLAGS'] = "".join(flags)
+                else:
+                    registers_values[words[1]] = binary(int(registers_values[words[2]], 2) - int(registers_values[words[3]], 2), 16)
+
+           
             elif words[0] == 'ld':  # load
                 registers_values[words[1]] = words[2][1::]
+           
             elif words[0] == 'st':  # store
                 variables[words[2]] = registers_values[words[1]]
-            elif words[0] == 'mul':
-                registers_values[words[1]] = registers_values[words[2]] * registers_values[words[3]]
+           
+            elif words[0] == 'mul':  # multiply
+                if int(registers_values[words[2]], 2) * int(registers_values[words[3]], 2) > 65535:
+                    flags = list(registers_values['FLAGS'])
+                    flags[12] = '1'
+                    registers_values['FLAGS'] = "".join(flags)
+                registers_values[words[1]] = binary(int(registers_values[words[2]], 2) * int(registers_values[words[3]], 2), 16)
+           
+            elif words[0] == 'not':
+                str=''
+                registers_values['FLAGS']='0000000000000000'
+                for i in registers_values[words[2]]:
+                    if(i=='1'):
+                        str+='0'
+                    else:
+                        str+'1' 
+                registers_values[words[1]]=str.copy() 
+
+            elif words[0] == 'cmp':
+                if(int(registers_values[words[1]])>int(registers_values[words[2]]) ):
+                    registers_values['FLAGS']='0000000000000010'
+                elif(int(registers_values[words[1]])<int(registers_values[words[2]]) ):
+                    registers_values['FLAGS']='0000000000000100'
+                else:
+                    registers_values['FLAGS']='0000000000000001'
+            elif words[0] == 'jmp':
+                pass
+            elif words[0] == 'jlt':
+                if(registers_values['FLAGS']!='0000000000000100'):
+                    # THIS LINE MUST PRODUCE AN ERROR>>>> I CAN'T UNDERSTAND, this error comes when we are getting a jlt statement and value of flag is not appropriate... so will that be an error or do we sim ply have to ignore this case 
+                    pass
+            elif words[0] == 'jgt':
+                pass
+            elif words[0] == 'je':
+                pass
+            elif words[0] =='hlt':
+                pass
+            
     else:
         if words[2][0] == '$':
             s += opcodes['mov1']
@@ -123,9 +291,7 @@ def process(command):
     # print(registers_values)
     # print(variables)
 
-
-with open('input.txt', 'rt') as inputfile:
-    command = inputfile.readline()
-    while command:
-        process(command)
-        command = inputfile.readline()
+for line in stdin:
+    if line == '':  # If empty string is read then stop the loop
+        break
+    check_errors(line)  # perform some operation(s) on given string
