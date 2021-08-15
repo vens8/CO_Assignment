98aa86ac825818c812fcff581766d36240905132
Sumit Soni <32272052+ssoni18@users.noreply.github.com>
2021-08-15 13:40:33 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,3 +1,4 @@
+from sys import float_repr_style, stdin
 opcodes = {
     'add': '00000',
     'sub': '00001',
@@ -58,31 +59,141 @@ registers = {
 
 # Initial register values
 registers_values = {
-    'R0': 0000000000000000,
-    'R1': 0000000000000000,
-    'R2': 0000000000000000,
-    'R3': 0000000000000000,
-    'R4': 0000000000000000,
-    'R5': 0000000000000000,
-    'R6': 0000000000000000,
-    'FLAGS': 0000000000000000,
+    'R0': '0000000000000000',
+    'R1': '0000000000000000',
+    'R2': '0000000000000000',
+    'R3': '0000000000000000',
+    'R4': '0000000000000000',
+    'R5': '0000000000000000',
+    'R6': '0000000000000000',
+    'FLAGS': '0000000000000000',
 }
-
+line_number=1
+mem_address=0
 # Dictionary to store the values of the memory addresses
-variables = {}
+variables={}  #its key will be the variable name or label name and value will be the memory address
+labels={}       #key will be the label name and value will be the memory address
+halt_instructions=[]#this will store the various number of halt statements encountered stored according to line number
+errors={}
+def parse(line):
+    global line_number
+    global mem_address
+    lst=list(line.split())
+    if(line=='\n'):
+        pass
+    else:
+        line_number+=1
+        if(lst[0][-1]==':'):
+            parse(line[len(lst[0])+1:])
+            labels[lst[0][:len(lst[0])-1]]=mem_address
+            mem_address+=1
+
+        elif(lst[0]=='var'):
+            if(len(lst)==2):
+                if(lst[1] in variables.keys() or lst[1] in labels.keys()):
+                    errors[line_number]="Wrong syntax used for instructions"
+                elif(lst[1] not in variables.keys() and lst[1] not in labels.keys()):
+                    variables[lst[1]]=mem_address
+                    mem_address+=1
+                    flag=True
+                else:
+                    errors[line_number]="wrong synatx used for instructions"
+            else:
+                errors[line_number]="wrong syntax used for instructions(wrong number of arguments are present)"
+        
+        elif(lst[0] in opcodes.keys()):  
+            if(lst[0]=='add' or lst[0]=='sub' or lst[0]=='mul' or lst[0]=='xor' or lst[0]=='or' or lst[0]=='and'):
+                if(len(lst)==4):   #to check the number of argumenst are correct or not
+                    if(lst[1] in registers.keys() and lst[2] in registers.keys() and lst[3] in registers.keys()):
+                        mem_address+=1
+                        flag=True
+                    else:
+                        errors[line_number]="wrong syntax used for instructions"
+                else:
+                    errors[line_number]="wrong syntax used for instructions(wrong number of arguments are present)"    
+                   
+            elif(lst[0]=='rs' or lst[0]=='ls'):
+                if(len(lst)==3):
+                    if(lst[1] in registers.keys() and lst[2] not in registers.keys()):
+                        if(lst[2][0]=='$' and lst[2][1:]>=0 and lst[2][1:]<=225):
+                            mem_address+=1
+                            flag=True
+                        else:
+                            errors[line_number]="wrong value of immidiate used or wrong syntax used for instructions"
+                    else:
+                        errors[line_number]="wrong syntax used for instructions"
+                else:
+                    errors[line_number]="wrong syantax used for instructions(wrong number of arguments are present" 
+                    
+            elif(lst[0]=='mov'):
+                if(len(lst)==3):
+                    if((lst[1] in registers and lst[2][0]=='$') or (lst[1] in registers and lst[2] in registers)):            
+                        if(lst[1] in registers and lst[2][0]=='$'):
+                            if(lst[2][1:]>=0 and lst[2][1:]<=225):
+                                mem_address+=1
+                                flag=True
+                            else:
+                                errors[line_number]="Illegal immidieate value is used"
+                        
+                        else:
+                            mem_address+=1
+                            flag=True
+                    else:
+                        errors[line_number]="Wrong type of arguments are used"
+                else:
+                    errors[line_number]="Wrong number of argumenst are used"
+            
+            elif(lst[0]=='ld' or lst[0]=='st'):
+                if(len(lst)==3):
+                    if(lst[1] in registers.keys() and lst[2] in labels.keys() and lst[2] not in variables.keys()):
+                        mem_address+=1
+                        flag=True
+                    else:
+                        errors[line_number]="wrong type of arguments are used"
+                else:
+                    errors[line_number]="wrong number of arguments are present"
+            
+            elif(lst[0]=='jmp' or lst[0]=='jlt' or lst[0]=='jgt' or lst[0]=='je'):
+                if(len(lst)==2):
+                    if(lst[1] not in labels.keys()):
+                        labels[lst[1]]=mem_address
+                    if(lst[1] in variables.keys()):
+                        errors[line_number]="wrong syntax used for instructions"
+                    else:
+                        mem_address+=1
+                        flag=True
+                else:
+                    errors[line_number]="wrong synatx for isntruction(Wrong number of argumensta are used"
+
+            elif(lst[0]=='hlt'):
+                if(len(lst)==1):
+                    halt_instructions.append(line_number)
+                    mem_address+=1
+                    flag=True
+                else:
+                    errors[line_number]="Wrong syntax used(incorrect number of arguments)"
+
+        else:
+            errors[line_number]='typos in instruction name'
+    return flag
 
 
 # Convert a string decimal into the equivalent binary of custom bits
 def binary(number, bit):
     binary = bin(int(number)).replace('0b', '')[::-1]
+    if len(binary) > bit:
+        binary = binary[0:bit]
+        return binary[::-1]
+
     while len(binary) < bit:
         binary += '0'
     return binary[::-1]
 
-
 def process(command):
     s = ""
     words = list(command.split())
+    registers_values['FLAGS']='0000000000000000'
+    
     if words[0] != 'mov':
         if words[0] == 'var':  # Create a new variable and store a default value as 0.
             variables[words[1]] = 0000000000000000
@@ -95,16 +206,64 @@ def process(command):
                 s += '0' * (16 - 5 - 3 * types[opcodes[words[0]]])
                 for i in range(1, types[opcodes[words[0]]] + 1):
                     s += registers[words[i]]
-            elif words[0] == 'add':  # addition
-                registers_values[words[1]] = registers_values[words[2]] + registers_values[words[3]]
+            
+            if words[0] == 'add':  # addition
+                if int(registers_values[words[2]], 2) + int(registers_values[words[3]], 2) > 65535:
+                    registers_values['FLAGS']='0000000000001000'
+                    registers_values[words[1]] = binary(int(registers_values[words[2]], 2) + int(registers_values[words[3]], 2), 16)  # Store only 16 bits even for overflow
+                    print(s)
+
             elif words[0] == 'sub':  # subtraction
-                registers_values[words[1]] = registers_values[words[2]] - registers_values[words[3]]
+                if int(registers_values[words[2]], 2) - int(registers_values[words[3]], 2) < 0:
+                    registers_values['FLAGS']='0000000000001000'
+                    registers_values[words[1]] = binary(int(registers_values[words[2]], 2) - int(registers_values[words[3]], 2), 16)
+                    print(s)
+
             elif words[0] == 'ld':  # load
                 registers_values[words[1]] = words[2][1::]
+                print(s)
+
             elif words[0] == 'st':  # store
                 variables[words[2]] = registers_values[words[1]]
-            elif words[0] == 'mul':
-                registers_values[words[1]] = registers_values[words[2]] * registers_values[words[3]]
+                print(s)
+
+            elif words[0] == 'mul':  # multiply
+                if int(registers_values[words[2]], 2) * int(registers_values[words[3]], 2) > 65535:
+                    registers_values['FLAGS']='0000000000001000'
+                    registers_values[words[1]] = binary(int(registers_values[words[2]], 2) * int(registers_values[words[3]], 2), 16)
+                    print(s)
+            
+            elif words[0] == 'not':
+                str=''
+                registers_values['FLAGS']='0000000000000000'
+                for i in registers_values[words[2]]:
+                    if(i=='1'):
+                        str+='0'
+                    else:
+                        str+='1' 
+                registers_values[words[1]]=str.copy() 
+                print(s)
+            elif words[0] == 'cmp':
+                if(int(registers_values[words[1]])>int(registers_values[words[2]]) ):
+                    registers_values['FLAGS']='0000000000000010'
+                elif(int(registers_values[words[1]])<int(registers_values[words[2]]) ):
+                    registers_values['FLAGS']='0000000000000100'
+                else:
+                    registers_values['FLAGS']='0000000000000001'
+            elif words[0] == 'jmp':
+                pass
+            elif words[0] == 'jlt':
+                if(registers_values['FLAGS']=='0000000000000100'):
+                    print(s)
+            elif words[0] == 'jgt':
+                if(registers_values['FLAGS']=='0000000000000010'):
+                    print(s)
+            elif words[0] == 'je':
+                if(registers_values['FLAGS']=='0000000000000001'):
+                    print(s)
+            elif words[0] =='hlt':
+                print(s)
+            
     else:
         if words[2][0] == '$':
             s += opcodes['mov1']
@@ -123,9 +282,8 @@ def process(command):
     # print(registers_values)
     # print(variables)
 
-
-with open('input.txt', 'rt') as inputfile:
-    command = inputfile.readline()
-    while command:
-        process(command)
-        command = inputfile.readline()
+for line in stdin:
+    if line == '':  # If empty string is read then stop the loop
+        break
+    else:
+        parse(line)  # perform some operation(s) on given string
