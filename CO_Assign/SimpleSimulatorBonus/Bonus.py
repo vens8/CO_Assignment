537e17bbb8547cb4c891add7a3fd21d558898b31
venkata20149 <88591222+venkata20149@users.noreply.github.com>
2021-08-21 23:34:36 +0530

    Update Bonus.py

--- CO_Assign/SimpleSimulatorBonus/Bonus.py
+++ CO_Assign/SimpleSimulatorBonus/Bonus.py
@@ -27,9 +27,9 @@ registers_values = {
     'FLAGS': '0000000000000000',
 }
 
-variables = {}   # a dictionary to store the register values and its mem_address
-mem_address, cycle = 0, 0   #initialized variables to store the number of cycles amd mem_address for every line
-x, y = [], []  # a pair of list to store the values of the above variables for the plotting of the Scatter plot graph
+variables = {}   # memory address of variables mapped to their value
+mem_address, cycle = 0, 0   # initialized variables to store the number of cycles amd mem_address for every line
+x, y = [], []  # a pair of lists to store the values of the above variables for the plotting of the scatter plot graph
 
 # Convert a string decimal into the equivalent binary of custom bits
 def binary(number, bit):
